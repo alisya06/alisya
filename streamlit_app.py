89b292a91df2ab1caed9ef6c89e4630c5ac33491
alisya06 <alisyaputeriramadhani@gmail.com>
2025-05-15 14:27:55 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.title("alisya‼️")
 st.write(
-    "hallo nasayy."
+    "hallo  nasayy."
 )
 
 st.image("https://github.com/alisya06/alisya/blob/main/views/20250424_124742.jpg")
