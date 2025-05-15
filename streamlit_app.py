830c81d5e7b92efcbec3351b452ed3ba33125226
alisya06 <alisyaputeriramadhani@gmail.com>
2025-05-15 14:40:42 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,6 @@
 import streamlit as st
 
-st.title("alisya‼️")
+st.title("alisya ‼️")
 st.image("20250515_110612.jpg",width=200)
-st.subheader("nazlan naik meja tukang cimol)
-st.write(
-    "jangan jadi kaya nazlan ya!"
-)
+st.subheader("nazlan naik meja tukang cimol")
+st.write("jangan jadi kaya nazlan ya!")
