869a1a3df8fd9c7f632a3d3391980a6877d869d2
alisya06 <alisyaputeriramadhani@gmail.com>
2025-05-22 11:33:49 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,12 @@ st.title("alisya ‼️")
 st.image("20250515_110612.jpg",width=200)
 st.subheader("nazlan naik meja tukang cimol")
 st.write("jangan jadi kaya nazlan ya!")
+
+#with col1:
+st.header("Aplikasi Mengecek Nilai Genap/Ganjil)
+angka = st.number_input("Tulis sebuah Angka:", value=0, step=1)
+
+if (angka % 2) == 0:
+  st.write(f"{angka} adalah Bilangan Genap")
+else:
+  st.write(f"{angka} adalah Bilangan Ganjil")
